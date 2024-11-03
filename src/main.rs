c0632d42538b3a51b47955dc45f0d166676fd456
Fanda Vacek <fvacek@elektroline.cz>
2024-11-03 17:58:04 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#[macro_use] extern crate rocket;
+
+use std::io;
+use std::path::{PathBuf};
+use std::sync::atomic::{AtomicI32, Ordering};
+use rocket::data::{Data, ToByteUnit};
+use rocket::response::content::RawText;
+use rocket::tokio::fs::{self, File};
+
+// In a real application, these would be retrieved dynamically from a config.
+// const HOST: Absolute<'static> = uri!("http://*:8000");
+const DATA_DIR: &'static str = "/tmp/qxhttpd/data";
+static RECENT_RECORD_ID: AtomicI32 = AtomicI32::new(0);
+
+type RecordId = i32;
+fn next_record_id() -> RecordId {
+    let old_id = RECENT_RECORD_ID.fetch_add(1, Ordering::SeqCst);
+    old_id + 1
+}
+fn record_file(id: RecordId) -> io::Result<PathBuf> {
+    std::fs::create_dir_all(DATA_DIR)?;
+    Ok(PathBuf::from(DATA_DIR).join(format!("{:04}", id)))
+}
+
+#[post("/", data = "<paste>")]
+async fn upload(paste: Data<'_>) -> io::Result<String> {
+    let id = next_record_id();
+    let file_path = record_file(id)?;
+    println!("file path: {:?}", file_path);
+    paste.open(128.kibibytes()).into_file(file_path).await?;
+    Ok(id.to_string())
+}
+
+#[get("/api/<id>")]
+async fn retrieve(id: RecordId) -> Option<RawText<File>> {
+    let Ok(file_path) = record_file(id) else { return None };
+    File::open(file_path).await.map(RawText).ok()
+}
+
+#[delete("/<id>")]
+async fn delete(id: RecordId) -> Option<()> {
+    let Ok(file_path) = record_file(id) else { return None };
+    fs::remove_file(file_path).await.ok()
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+
+      POST /
+
+          accepts raw data in the body of the request and responds with a URL of
+          a page containing the body's content
+
+          EXAMPLE: curl --data-binary @file.txt http://localhost:8000
+
+      GET /<id>
+
+          retrieves the content for the paste with id `<id>`
+    "
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, upload, delete, retrieve])
+}
